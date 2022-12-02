af621c6904cb6d2f099c2e53bf5298f8bce393b2
IvanDzida <dzida.ivan@rolla.app>
2022-12-02 21:11:51 +0100

    Implemented splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
 
 void main() {
-  runApp(const MyApp());
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
+  runApp(const ProviderScope(child: MyApp()));
+
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
